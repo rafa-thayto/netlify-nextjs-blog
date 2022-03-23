fa1cd69f5680c5a73960980b529dba32ef644988
Rafael Thayto <rafa.thayto@gmail.com>
2022-03-23 00:03:12 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script src="/static/test.js" />
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
